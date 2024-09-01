834bfbbb2666477be5d5c02c4d9df6f3895172dc
DonglingMo <donglingmo93@gmail.com>
2024-09-01 22:36:35 +0800

    init commit

--- LinkedList/src/main/java/leetcode/demo/LeetCodeNo2.java
+++ LinkedList/src/main/java/leetcode/demo/LeetCodeNo2.java
@@ -1,2 +1,88 @@
-package leetcode.demo;public class LeetCodeNo2 {
+package leetcode.demo;
+
+
+public class LeetCodeNo2 {
+
+    /**
+     * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+     *
+     * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+     *
+     * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     *
+     * 每个链表中的节点数在范围 [1, 100] 内
+     * 0 <= Node.val <= 9
+     * 题目数据保证列表表示的数字不含前导零
+     *
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+
+    public ListNode addZTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 == null ? 0 : l1.val;
+            int n2 = l2 == null ? 0 : l2.val;
+            // 低位相加
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                // 取低位
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            //获取进位
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            if (carry > 0) {
+                tail.next = new ListNode(carry);
+            }
+        }
+        return head;
+    }
+
+
+    private static class ListNode {
+        int val;
+        public ListNode next;
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode() {
+        }
+
+        @Override
+        public String toString() {
+            return "ListNode{" +
+                    "val=" + val +
+                    ", next=" + next +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        LeetCodeNo2 leetCodeNo2 = new LeetCodeNo2();
+        ListNode l1 = new ListNode(5, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+        ListNode listNode = leetCodeNo2.addZTwoNumbers(l1, l2);
+        System.out.println(listNode);
+    }
 }
+
